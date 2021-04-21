e0d2fd342377b860896cf3673949d79f040da0f2
Andiswamy <andiswamy.raja@crystaldelta.com>
2021-04-21 21:40:56 +0530

    Addedd drag and resize event function for Rnd

--- src/oppanai/views/oppanai-editor.tsx
+++ src/oppanai/views/oppanai-editor.tsx
@@ -7,10 +7,14 @@ interface OppanaiEditorProps {
 }
 
 class OppanaiEditor extends React.Component<OppanaiEditorProps> {
+    state = {
+        imageWidth: 0,
+        imageHeight: 0
+    }
     render() {
         return (
             <div className='d-flex justify-content-center oppanai-editor-wrapper preview-on-edit' >
-                <div className=' align-items-center'>
+                <div className=' align-items-center my-image'>
                     <Image src={this.props.imageSource} className='mw-100 oppanai-box-shadow oppanai-hero-image' />
                     <RND />
                 </div>
