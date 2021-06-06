53d48ce95dabf27b9aae015c445ba3b54f1b1f20
Andiswamy <andiswamy.raja@crystaldelta.com>
2021-06-06 10:17:45 +0530

    Rotate image UT is done

--- src/oppanai/unitTests/rotateImages.test.tsx
+++ src/oppanai/unitTests/rotateImages.test.tsx
@@ -2,12 +2,21 @@ import { shallow } from 'enzyme';
 import OppanaiEditor from '../views/oppanai-editor';
 import RotateImage from '../views/rotateImage';
 import { mockData } from './mockData';
+import * as actions from '../utils/oppanaiActions';
 
 describe('Given in Oppanai Editor', () => {
     global.URL.createObjectURL = jest.fn();
     const oppanaiEditor = new OppanaiEditor({ imageSource: mockData.imageBlobUrl, imageFile: mockData.file });
-    // const oppanaiEditorWrapper = shallow(<OppanaiEditor imageSource={mockData.imageBlobUrl} imageFile={mockData.file} />)
-    const rotateImageWrapper = shallow(<RotateImage handleRotateImage={oppanaiEditor.handleRotateImage} />)
+    const oppanaiEditorWrapper = shallow(<OppanaiEditor imageSource={mockData.imageBlobUrl} imageFile={mockData.file} />)
+    const rotateImageWrapper = shallow(<RotateImage handleRotateImage={oppanaiEditor.handleRotateImage} />);
+
+    const handleRotateStyles = (angle: number) => {
+        oppanaiEditorWrapper.setState({
+            rotateStyle: angle
+        });
+        oppanaiEditorWrapper.childAt(0).childAt(0).simulate('click');
+    }
+
     describe('When user tries rotating an image', () => {
         test('Test the Edit options are rendered', () => {
             expect(rotateImageWrapper.find('.oppanai-rotate-wrapper')).not.toBeNull();
@@ -17,12 +26,33 @@ describe('Given in Oppanai Editor', () => {
         });
         test('Test the handleRotateImage function called on clicking right rotate', () => {
             // const spy = jest.spyOn(oppanaiEditorWrapper.instance(), 'handleRotateImage');
-            console.log(rotateImageWrapper.childAt(0).childAt(0));
             rotateImageWrapper.childAt(0).childAt(0).simulate('click');
             rotateImageWrapper.childAt(1).childAt(0).simulate('click');
             rotateImageWrapper.childAt(2).childAt(0).simulate('click');
             rotateImageWrapper.childAt(3).childAt(0).simulate('click');
             // expect(spy).toBeCalledTimes(2);
+        });
+    });
+    describe('When user tries downloading edited image', () => {
+        test('Check Download button present', () => {
+            expect(oppanaiEditorWrapper.childAt(0).childAt(0)).not.toBeNull();
+            expect(oppanaiEditorWrapper.childAt(0).childAt(0).type()).toBe('label');
+            expect(oppanaiEditorWrapper.childAt(0).childAt(0).props().className).toEqual('oppanai-download');
+            expect(oppanaiEditorWrapper.childAt(0).childAt(0).childAt(0).props().id).toEqual('oppanai-download-image');
+        });
+        test('simulate end verify the download starts on clicking it', () => {
+            const downloadSpy = jest.spyOn(actions, 'handleDownload');
+            oppanaiEditorWrapper.childAt(0).childAt(0).simulate('click');
+            expect(downloadSpy).toBeCalledTimes(1);
+        });
+        test('Check with different rotate styles', () => {
+            const downloadSpy = jest.spyOn(actions, 'handleDownload');
+            handleRotateStyles(90);
+            expect(downloadSpy.mock.calls[0][2]).toBe(90);
+            handleRotateStyles(180);
+            expect(downloadSpy.mock.calls[1][2]).toBe(180);
+            handleRotateStyles(270);
+            expect(downloadSpy.mock.calls[2][2]).toBe(270);
         });
     });
 });
--- src/oppanai/utils/oppanaiActions.ts
+++ src/oppanai/utils/oppanaiActions.ts
@@ -5,21 +5,21 @@ var zoomOutCount = 0;
 export const handleDownload = (imageRef: any, imageFile: HTMLImageElement, rotateStyle: number) => {
     const editedImage: any = document.getElementById(CONSTANTS.OPPANAI_EDITING_IMAGE);
     const canvas = document.createElement('canvas');
-    canvas.width = (editedImage.style.transform.includes(90) || editedImage.style.transform.includes(270)) ? imageRef.naturalHeight : imageRef.naturalWidth;
-    canvas.height = (editedImage.style.transform.includes(90) || editedImage.style.transform.includes(270)) ? imageRef.naturalWidth : imageRef.naturalHeight;
+    canvas.width = (editedImage?.style.transform.includes(90) || editedImage?.style.transform.includes(270)) ? imageRef.naturalHeight : imageRef.naturalWidth;
+    canvas.height = (editedImage?.style.transform.includes(90) || editedImage?.style.transform.includes(270)) ? imageRef.naturalWidth : imageRef.naturalHeight;
     const ctx: any = canvas.getContext('2d');
     //Define translation to place rotate position
-    if (rotateStyle === 90) ctx.translate(canvas.width, 0);
-    else if (rotateStyle === 180) ctx.translate(canvas.width, canvas.height);
-    else if (rotateStyle === 270) ctx.translate(0, canvas.height);
-    else ctx.translate(0, 0)
+    if (rotateStyle === 90) ctx?.translate(canvas.width, 0);
+    else if (rotateStyle === 180) ctx?.translate(canvas.width, canvas.height);
+    else if (rotateStyle === 270) ctx?.translate(0, canvas.height);
+    else ctx?.translate(0, 0)
     //Rotate image by converting Degree into Radian
-    ctx.rotate(rotateStyle * Math.PI / 180);
-    ctx.drawImage(
+    ctx?.rotate(rotateStyle * Math.PI / 180);
+    ctx?.drawImage(
         imageRef,
         0, 0
     )
-    ctx.restore();
+    ctx?.restore();
     //start download the edited image
     startImagedownload(imageFile.name.split('.')[0], canvas.toDataURL());
     return canvas.toDataURL();
